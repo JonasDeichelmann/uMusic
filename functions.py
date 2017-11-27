18a5315209b7bcb6bd246bace44665817dd0ca7a
JonasDeichelmann <Jonas.Deichelmann@me.com>
2017-11-27 11:45:51 -0800

    Working on the GenerateNumber for the Music function, after user input

--- functions.py
+++ functions.py
@@ -2,6 +2,18 @@
 #File: root.py
 #Class: CST205
 #Created by: Jonas Deichelmann
-#Description: This is the function Source file for the final project
+#Description: This is the function Source file for the final project. The File get the Input String from the user and works with this
 #Created on: 11/21/2017
 #Last change: 11/21/2017
+import random
+import math
+
+out = []
+inp=input()
+for i in inp:
+    out.append(int(math.sqrt(math.pow(ord(i), 3))))
+for j in range(len(out)-1):
+    randomNumber = int(random.uniform(out[j],out[j-1]))
+    print(randomNumber)
+print(out)
+#return out
