569e9d6925c6c478c2f5b559928f0ffa2eaff77c
JonasDeichelmann <Jonas.Deichelmann@me.com>
2017-11-29 10:15:57 -0800

    Working on the root.py file

--- root.py
+++ root.py
@@ -8,7 +8,6 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-import random
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -16,7 +15,6 @@ Bootstrap(app)
 @app.route('/')
 @app.route('/index')
 def template_func():
-    randomNumber = int(random.uniform(0,10))
-    print(randomNumber)
+
     #create randomNumber for the next Step, when we want to create random music
     return render_template('template.html')
